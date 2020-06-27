32a9c038a3036a10eee274d3f5679ad50b5a96c8
Hope Gipson <gipson.hope@gmail.com>
2020-06-27 01:58:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  return programmer_hash[:dennis_ritchie][:languages][0]
+
 end
 
 def adding_matz
@@ -80,7 +83,8 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash [:yukihiro_matsumoto] = [:known_for=>"Ruby", :languages=>["LISP", "C"]]
+    return programmer_hash
 end
 
 def changing_alan
